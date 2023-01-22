9e7a97459f5008e362df577e963bfd1869e9ea47
kulgg <75735874+kulgg@users.noreply.github.com>
2023-01-22 20:56:25 +0100

    Refactor UI

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        screens: {
+          sm: "100%",
+          md: "100%",
+          lg: "800px",
+          xl: "800px",
+        },
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
