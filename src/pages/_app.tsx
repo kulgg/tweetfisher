17853579b28caafa2cd7d4028993d7e74b47ae82
kulgg <75735874+kulgg@users.noreply.github.com>
2023-01-23 13:05:48 +0100

    Add react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "../styles/globals.css";
 
+const queryClient = new QueryClient()
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (<QueryClientProvider client={queryClient}>
+    <Component {...pageProps} />
+  </QueryClientProvider>)
 };
 
 export default MyApp;
