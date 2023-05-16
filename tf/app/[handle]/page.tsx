2c0b36a056608153cb4fea36c72bc6d39d43d826
kulgg <75735874+kulgg@users.noreply.github.com>
2023-05-16 20:58:47 +0200

    Add Tweet component with suspense

--- tf/app/[handle]/page.tsx
+++ tf/app/[handle]/page.tsx
@@ -1,4 +1,7 @@
+import Tweet from "@/components/tweet";
+import { Skeleton } from "@/components/ui/skeleton";
 import { groupByUrl, validUrlsFilter } from "@/lib/utils";
+import { Suspense } from "react";
 
 async function getUnique(data: any) {
   return data.filter(validUrlsFilter).reduce(groupByUrl, {});
@@ -22,8 +25,21 @@ async function Page({ params }: { params: { handle: string } }) {
   const archivedTweets = await archivedTweetsResponse.json();
   const tweetToArchivesMap = await getUnique(archivedTweets);
 
+  console.log(tweetToArchivesMap[Object.keys(tweetToArchivesMap).at(0)!][0]);
+
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen max-w-[670px] mx-auto mt-20">
+      <Suspense
+        fallback={
+          <Skeleton className="w-[670px] h-[300px] bg-slate-100 dark:bg-slate-800" />
+        }
+      >
+        <Tweet
+          archive={
+            tweetToArchivesMap[Object.keys(tweetToArchivesMap).at(0)!][0]
+          }
+        />
+      </Suspense>
       <div>{archivedTweets.length}</div>
       <div>{Object.keys(tweetToArchivesMap).length}</div>
       {JSON.stringify(accountStatus)}
